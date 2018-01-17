d0c9a1a72d981e0061db3537a7528087409aa3fa
Dreddsparc <sparc399@gmail.com>
2018-01-16 18:36:24 -0600

    Beginning of the Main Object file for the game. TttGame defined.  setup_players() method in place to init the player dictionary, and get_current_player method working.
    lots more to be done though. uggh

--- scratch_obj.py
+++ scratch_obj.py
@@ -1,48 +1,62 @@
 import random as rnd
 
 
-def player_setup():
+class TttGame(object):
     """
-    Basic function to setup the initial player dictionary with names, and player tokens
-    and a turn indicator.
-
-    player1 and player2 are keys pointing to sub-dictionaries with name, token and turn
-    status.
-
-    Takes no arguments and returns a dictionary as defined below
-
-    ToDo : Add input checks to be sure name that are entered make sense
+    Tic-Tac-ToeJam Main Game Object
     """
+    # Class Variables
+    Debug = False
     player_dict = {"player1": {'name': 'AName', 'token': 'X', 'turn': 0},
                    "player2": {'name': 'AName', 'token': 'O', 'turn': 0}}
-    # Get the players names into the dictionary
-    for i in range(1, 3):
-        player_dict['player' + str(i)]['name'] = input("Please enter the name of player {0}: ".format(i))
 
-    # Decide which player will start first
-    player_dict['player1']['turn'] = rnd.randint(0, 1)
-    player_dict['player2']['turn'] = abs(player_dict['player1']['turn'] - 1)
+    def __init__(self):
+        pass
 
-    return player_dict
-# End player_setup()
+    @staticmethod
+    def toggle_debug():
+        """
+        Toggle Debug flag to show or suppress extra console output while building class
+        :return: Return Debug status, True if on, False if Off
+        """
+        if TttGame.Debug:
+            TttGame.Debug = False
+            print("Setting Debug to False")
+        else:
+            TttGame.Debug = True
+            print("Setting Debug to True")
+        return TttGame.Debug
 
+    def setup_players(self):
+        """
+        Setup the player Dictionary class variable for use in the game
+            Dict keys are 'player1', 'player2'
+            Player Dict Keys are 'name', 'token', 'turn'
+                name : name player entered in setup
+                token: token player will use in the game (x or o)
+                turn : flag that indicates if its the players turn 1 for current player 0 if not
+        :return: No return value
+        """
+        for i in range(1, 3):
+            TttGame.player_dict['player' + str(i)]['name'] = input("Please enter the name of player {0}: ".format(i))
 
-class TttGame:
-
-
-    def __init__(self, player_dict):
-        self.player_dict = player_dict
+        # Decide which player will start first
+        TttGame.player_dict['player1']['turn'] = rnd.randint(0, 1)
+        TttGame.player_dict['player2']['turn'] = abs(TttGame.player_dict['player1']['turn'] - 1)
+        if TttGame.Debug:
+            print(TttGame.player_dict)
 
     def get_player1_name(self):
-        return self.player_dict['player1']['name']
-    pass
+        return TttGame.player_dict['player1']['name']
 
     def get_current_player(self):
         for key in self.player_dict.keys():
             if self.player_dict[key]['turn'] == 1:
-                return self.player_dict[key]
+                return TttGame.player_dict[key]['name']
 
 
-theGame = TttGame(player_setup())
-print(theGame.get_player1_name())
-print(theGame.get_current_player()['name'])
+theGame = TttGame()
+
+theGame.toggle_debug()
+theGame.setup_players()
+print(theGame.get_current_player())
